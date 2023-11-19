640c08a4bb3e030829e5e0ff2df9af4c46e536b0
Massyl <massyl.d@live.fr>
2023-11-19 14:00:07 +0100

    Mise en place de point de terminaison EXO 1

--- app.js
+++ app.js
@@ -1,8 +1,24 @@
 // console.log("salut tout le monde");
 const express = require("express"); //recuperation du paquet express
+let pokemons = require("./mock-pokemon");
 const app = express(); //instace de express (serveur web ou va fonction notre API REST
 const port = 3000;
-app.get("/", (req, res) => res.send("salut, express"));
+//HOME PAGE
+app.get("/", (req, res) => res.send("salut, express!"));
+//ajout d'un nouveau point de terminaison
+//POKEMONS PAGE
+app.get("/api/pokemons/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+  res.send(`vous avez demande le pokemon ${pokemon.name} !`);
+});
+//EXO:mettre un nouveau point de terminaison qui retournera le nombre total de pokemon present dans notre API RESt
+app.get("/api/pokemons", (req, res) => {
+  res.send(
+    `il y a ${pokemons.length} pokemons dans le pokedex, pour le moment!`
+  );
+});
+
 app.listen(
   port,
   console.log(`notre application node a démarée sur: http://localhost:${port}`)
