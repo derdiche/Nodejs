1737792218a0ee3cd42f98ad52f818b51328f5f8
Massyl <massyl.d@live.fr>
2023-11-20 14:27:25 +0100

    Les middlewares d'Express

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 // console.log("salut tout le monde");
 const express = require("express"); //recuperation du paquet express
 let pokemons = require("./mock-pokemon"); // recuperation des données
-const { succes } = require("./helper.js"); // recuperation des messages
+const { succes } = require("./helper.js"); // recuperation destrucuré de la methode succes
 /*                      IMPORT                      */
 const app = express(); //instace de express (serveur web ou va fonction notre API REST
 const port = 3000;
+//Middleware
+
+app.use((req, res, next) => {
+  //middleware qui affichera les requete recu par l'API REST
+  console.log(`url: ${req.url}`);
+  next(); //terminaison du middleware
+});
 //HOME PAGE
 app.get("/", (req, res) => res.send("salut, express!"));
 //ajout d'un nouveau point de terminaison
@@ -19,11 +26,16 @@ app.get("/api/pokemons/:id", (req, res) => {
   // res.send(`vous avez demande le pokemon ${pokemon.name} !`);//CETTE METHODE NE REVOI PAS JSON FORMAT STANDAR
   res.json(succes(message, pokemon));
 });
-//EXO:mettre un nouveau point de terminaison qui retournera le nombre total de pokemon present dans notre API RESt
+
+//EXO1:mettre un nouveau point de terminaison qui retournera le nombre total de pokemon present dans notre API RESt
+// app.get("/api/pokemons", (req, res) => {
+//   res.send(
+//     `il y a ${pokemons.length} pokemons dans le pokedex, pour le moment!`
+//   );
+// });
+//EXO2:renvoyer la liste des pokemons
 app.get("/api/pokemons", (req, res) => {
-  res.send(
-    `il y a ${pokemons.length} pokemons dans le pokedex, pour le moment!`
-  );
+  res.send(succes("La liste des pokemon a bien été recuperée", pokemons));
 });
 
 app.listen(
