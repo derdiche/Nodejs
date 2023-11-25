64d833840c54d59ea2feeabd0bc831ac7b3aa738
Massyl <massyl.d@live.fr>
2023-11-25 12:11:50 +0100

    backend terminé

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ sequelize.initDb();
 require("./src/routes/findAllPokemons")(app);
 require("./src/routes/findPokemonByPk")(app);
 require("./src/routes/createPokemon")(app);
+require("./src/routes/updatePokemon")(app);
+require("./src/routes/deletePokemon")(app);
 app.listen(
   port,
   console.log(`notre application node a démarée sur: http://localhost:${port}`)
